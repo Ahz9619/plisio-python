ff52ecf46762ce21740c858413c6dccd59404d57
Ahz9619 <phanomhongaek72@gmail.com>
2025-01-10 10:04:15 +0700

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import plisio
 
 client = plisio.PlisioClient(api_key='aLU30JL9c1yeLp-XF7rsjoigB9SjIR9WaGDMcj8YImRPIsPDYVqyEz_4LMkapuFx')
 
-# balance = client.get_operation('64d1df01224bd682be0c12c4')
+# balance = $99999 client.get_operation('64d1df01224bd682be0c12c4')
 second_invoice = client.create_invoice(
     plisio.CryptoCurrency.TRX,
     'TRXX6',
